6b8f1a15b8865df711840227ec40eb4d57785921
sierratp <sierratp16@gmail.com>
2023-07-14 09:17:35 -0700

    added send_config

--- netConnectionCopy.py
+++ netConnectionCopy.py
@@ -3,20 +3,15 @@ from netmiko import ConnectHandler
 
 
 
-
+# prints the server and description; more-so for a check to make sure the code parses the right data
 def netConnection(sHost,description):
     
     with open('output.csv', 'a', newline='') as o:
         outfile = csv.writer(o) # removes quotation around description =  quoting=csv.QUOTE_NONE, escapechar=' '
         #outfile.writerow([sHost, description])
     
-    
-
-        net_connect = ConnectHandler(device_type="",host= sHost,username="",password="",)
-        output = net_connect.send_command("show ip arp")
-
         #print(output) 
-        outfile.writerow([output])
+
 
 
 
@@ -24,7 +19,7 @@ def netConnectionTestReader():
     
     print("reading server...")
     count = 0
-    with open('data1.csv', 'r') as file:
+    with open('sample8thFloor.csv', 'r') as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
@@ -34,8 +29,15 @@ def netConnectionTestReader():
             sHost = row[3]
             description = row[7]
             
-            netConnection(sHost,description)
-            #print(sHost)
+            #netConnection(sHost,description)
+
+            # sets device equal to 
+            net_connect = ConnectHandler(device_type="cisco_xe",host= sHost,username="nettools",password="jrfoO6wPR3eGnoHC8Sh",)
+            #output = net_connect.send_command("show ip arp")
+
+            # configuration 
+            net_connect.send_config_set()
+            net_connect.save_config()
     print("finished printing to outfile")   
     print(count)   
     
